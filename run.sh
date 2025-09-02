99da8e904aade932f75b572a445ff0a7beb4cf61
R.G <rgudchenko@ya.ru>
2025-09-03 02:54:13 +0300

    best  rendering + best graph summary

--- run.sh
+++ run.sh
@@ -13,10 +13,10 @@ python -m guca.cli.run_ga_hydra \
   n_workers=8
 
 python -m guca.cli.run_ga_hydra \
-  ga.generations=1000 ga.pop_size=500 \
+  ga.generations=1000 ga.pop_size=100 \
   ga.structural.insert_pb=0.35 ga.structural.duplicate_pb=0.20 \
   ga.field.enum_delta_pb=0.25 \
-  machine.max_steps=100 \
+  machine.max_steps=10 \
   ga.checkpoint.save_population=all \
   ga.checkpoint.fmt=yaml \
   n_workers=12
