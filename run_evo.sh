cddf32d92a0e7b75e982ecbc29069c45b2465718
R.G <rgudchenko@ya.ru>
2025-09-14 21:28:50 +0300

    exp003.09 configs

--- run_evo.sh
+++ run_evo.sh
@@ -1,6 +1,32 @@
 export ML_LOGBOOK_DIR="_logbook"
 
-python -m guca.cli.run_ga_hydra --config-path=../../../configs/phase01 --config-name=exp003.08 \
-  experiment.name="exp003.08.03_tregen100K" \
+python -m guca.cli.run_ga_hydra --config-path=../../../configs/phase01 --config-name=exp003.09 \
+  experiment.name="exp003.09.01_tregen5K" \
+  ga.generations=5000 ga.pop_size=200 \
+  ga.checkpoint.save_every=1000
+
+python -m guca.cli.run_ga_hydra --config-path=../../../configs/phase01 --config-name=exp003.09 \
+  experiment.name="exp003.09.02_tregen10K" \
+  ga.generations=10000 ga.pop_size=200 \
+  ga.checkpoint.save_every=1000
+
+python -m guca.cli.run_ga_hydra --config-path=../../../configs/phase01 --config-name=exp003.09 \
+  experiment.name="exp003.09.03_tregen10K" \
+  ga.generations=10000 ga.pop_size=200 \
+  ga.checkpoint.save_every=1000
+
+python -m guca.cli.run_ga_hydra --config-path=../../../configs/phase01 --config-name=exp003.09 \
+  experiment.name="exp003.09.04_tregen10K" \
+  ga.generations=10000 ga.pop_size=200 \
+  ga.checkpoint.save_every=1000
+
+python -m guca.cli.run_ga_hydra --config-path=../../../configs/phase01 --config-name=exp003.09 \
+  experiment.name="exp003.09.05_tregen10K" \
+  ga.generations=10000 ga.pop_size=200 \
+  ga.checkpoint.save_every=1000
+
+
+python -m guca.cli.run_ga_hydra --config-path=../../../configs/phase01 --config-name=exp003.09 \
+  experiment.name="exp003.09.06_tregen100K" \
   ga.generations=100000 ga.pop_size=200 \
-  ga.checkpoint.save_every=5000
+  ga.checkpoint.save_every=10000
