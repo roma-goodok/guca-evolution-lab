1c42b1fd0209b9e6cf30620c2f40c79063bb153c
R.G <rgudchenko@ya.ru>
2025-09-15 15:36:13 +0300

    (!) trimesh with 64 deg, 100+ triangles (exp09)

--- run_evo_exp10.sh
+++ run_evo_exp10.sh
@@ -28,5 +28,5 @@ python -m guca.cli.run_ga_hydra --config-path=../../../configs/phase01 --config-
 
 python -m guca.cli.run_ga_hydra --config-path=../../../configs/phase01 --config-name=exp003.10 \
   experiment.name="exp003.10.06_tregen100K" \
-  ga.generations=100000 ga.pop_size=200 \
-  ga.checkpoint.save_every=10000
+  ga.generations=10000 ga.pop_size=200 \
+  ga.checkpoint.save_every=1000
